a59dbc1ab0f1f7b956c0c2b85cc91e0e06b77da4
root <root@9ce9c5475ab2>
2019-01-05 03:42:10 +0000

    core: Fix issues with setup  services

--- src/wordpress/wp-content/themes/onelove/woocommerce/single-product/shippingbox.php
+++
@@ -1,5 +0,0 @@
-<div class="cata-cols-wrapper cols-3">
-	<div class="cata-shippingbox col"><i class="ti-package"></i><span><?php esc_html_e('Free Shipping', 'onelove'); ?></span></div>
-	<div class="cata-shippingbox col"><i class="ti-truck"></i><span><?php esc_html_e('Delivery & Return', 'onelove'); ?></span></div>
-	<div class="cata-shippingbox col"><i class="ti-id-badge"></i><span><?php esc_html_e('Member Discount', 'onelove'); ?></span></div>
-</div>
